ecec62b15cde6b5847c4d1450b0f5c14a23a93c7
saydunnesa-shirin <shirin071989@gmail.com>
2024-10-04 13:36:49 +0300

    Added new student status (#27)

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -5,7 +5,7 @@ export const MODAL_SIZE_MEDIUM = 'M';
 export const baseURL= 'https://localhost:7080/api';
 export const LOGGED_IN_USER_ID = 1;
 export const EMPLOYEE_DESIGNATION_ID = 3;
-export const STUDENT_STATUS_IDS = [1, 2];
+export const STUDENT_STATUS_IDS = [1, 2, 4];
 
 
 //'https://localhost:7080/api';//'https://myschoolapp001.azurewebsites.net/api'; //'http://localhost:3006';
